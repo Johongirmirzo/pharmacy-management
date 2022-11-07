058d240e89c5f2b318551b180e17b9cff0f5cb2d
Johongirr <johongir_rahimov@bk.ru>
2022-11-07 10:38:07 +0100

    fix: fix not found dotenv error

--- api/app/server.ts
+++ api/app/server.ts
@@ -27,7 +27,7 @@ app.use(cors({
         if (!origin) return callback(null, true);
      
         if (allowedDomains.indexOf(origin) === -1) {
-          var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+          var msg = `This site ${origin} does not have an access!. Only specific domains are allowed to access it.`;
           return callback(new Error(msg), false);
         }
         console.log("Access control enabled", allowedDomains.indexOf(origin))
--- api/build/server.js
+++ api/build/server.js
@@ -37,7 +37,7 @@ app.use((0, cors_1.default)({
         if (!origin)
             return callback(null, true);
         if (allowedDomains.indexOf(origin) === -1) {
-            var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+            var msg = `This site ${origin} does not have an access!. Only specific domains are allowed to access it.`;
             return callback(new Error(msg), false);
         }
         console.log("Access control enabled", allowedDomains.indexOf(origin));
