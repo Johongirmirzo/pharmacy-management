5ba98a97f7c4ab3d5ab80650fe4c2b01e2f531bb
Johongirr <johongir_rahimov@bk.ru>
2022-12-20 14:32:58 +0100

    fix: remote text

--- admin/src/components/Loader/index.tsx
+++ admin/src/components/Loader/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import {BounceLoader} from "react-spinners"
-=======
 import { BounceLoader } from "react-spinners";
->>>>>>> 39948c6 (feature: add loading animation)
 import { LoaderBox } from "./index.styled";
 
 const Loader = () => {
